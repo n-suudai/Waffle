a149d37ce50698af68fcba034a82e3899bd4b408
suudai <34059792+n-suudai@users.noreply.github.com>
2020-11-18 00:45:21 +0900

    プロジェクトの追加

---
+++ source/engine/core/core.cpp
@@ -0,0 +1,15 @@
+#include "core.h"
+
+namespace engine {
+namespace core {
+
+bool initialize()
+{
+    return true;
+}
+
+void finalize()
+{}
+
+} // namespace core
+} // namespace engine
---
+++ source/engine/core/core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace engine {
+namespace core {
+
+bool initialize();
+void finalize();
+
+} // namespace core
+} // namespace engine
+
