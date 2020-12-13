371b3201e6b8c9c4ee418e2508f6a58eb6764b32
suudai <34059792+n-suudai@users.noreply.github.com>
2020-12-14 00:04:55 +0900

    キーボード用のインターフェイスを追加

---
+++ include/hid/keyboard.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+
+#include "hid/peripheral_device.h"
+#include "hid/key_code.h"
+
+
+namespace waffle {
+namespace hid {
+
+
+class IKeyBoard : public IPeripheralDevice
+{
+public:
+    virtual ~IKeyBoard() = default;
+
+    virtual bool isPressed(KeyCode key) const = 0;
+
+    virtual bool isReleased(KeyCode key) const = 0;
+
+    virtual bool isFirstPressed(KeyCode key) const = 0;
+
+    virtual bool isFirstReleased(KeyCode key) const = 0;
+
+    virtual wfl::chrono::microseconds getDurationPressed(KeyCode key) const = 0;
+};
+
+
+} // namespace hid
+} // namespace waffle
+
---
+++ include/hid/peripheral_device.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+
+#include "hid/entity.h"
+#include <chrono>
+
+
+namespace waffle {
+namespace hid {
+
+
+class IPeripheralDevice : public HIDEntity
+{
+public:
+    virtual ~IPeripheralDevice() = default;
+
+    virtual void update(wfl::chrono::microseconds deltaTime) = 0;
+
+    virtual bool isAnyPressed() const = 0;
+};
+
+
+} // namespace hid
+} // namespace waffle
+
