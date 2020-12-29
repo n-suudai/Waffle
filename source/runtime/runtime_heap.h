1d5835548a4f01cb4881d86f3cce936c977636bb
suudai <34059792+n-suudai@users.noreply.github.com>
2020-12-30 00:31:22 +0900

    runtime_heapをインスタンス化できないように

--- source/runtime/runtime_heap.h
+++ source/runtime/runtime_heap.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include "common/utility/non_instantiatable.h"
+
 
 namespace waffle {
 namespace runtime {
@@ -7,6 +9,7 @@ namespace runtime {
 
 class RuntimeHeap
 {
+    WFL_NON_INSTANTIATABLE(RuntimeHeap);
 public:
     static void printDebug_Report_MemoryAll();
     static void printDebug_Report_HeapTreeStats();
